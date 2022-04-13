2e2b70af76ce3fef6bf84558c7fe1c61ec97e3b2
Alexander Leino <Alexander.Leino@yahoo.com>
2022-04-13 12:51:19 -0400

    slider working

---
+++ index.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("click", e => {
+    let handle 
+    if(e.target.matches(".handle")) {
+        handle = e.target
+    } else {
+        handle = e.target.closest(".handle")
+    }
+    if (handle != null) {
+        console.log(handle)
+        onHandleClick(handle)
+    }
+})
+
+function onHandleClick(handle) {
+    const slider = handle.closest(".container").querySelector(".slider")
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
+    console.log(slider)
+    if(handle.classList.contains("left-handle")) {
+        slider.style.setProperty("--slider-index", sliderIndex - 1)
+    }
+    if(handle.classList.contains("right-handle")) {
+        slider.style.setProperty("--slider-index", sliderIndex + 1)
+    }
+}
